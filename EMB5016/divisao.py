0c30da3b8f870a8cb1b4e10790e1ff7189f1b83d
luizgcordeiro <48261336+luizgcordeiro@users.noreply.github.com>
2022-06-14 18:07:29 -0300

    yeahuj

---
+++ EMB5016/divisao.py
@@ -0,0 +1,29 @@
+#Teste para ver o quao mais lento que divisao é que multiplicacao
+
+import numpy as np
+import time
+
+n=10000000
+
+x=np.random.random(n)
+y=np.random.random(n)
+
+start_time=time.time()
+
+for i in range(n):
+    x[i]*y[i]
+#end for
+
+t=time.time()-start_time
+
+print("Tempo para " + str(n) + " multiplicações: " + str(t) + " segundos.")
+
+start_time=time.time()
+
+for i in range(n):
+    1/x[i]
+#end for
+
+t=time.time()-start_time
+
+print("Tempo para " + str(n) + " divisoes: " + str(t) + " segundos.")
