22918b8832ac9bb98a1f7484c65de3535fcca0e2
Luiz Cordeiro <luizgc6@gmail.com>
2022-07-18 15:32:40 -0300

    small stuff

--- EMB5005/main.py
+++ EMB5005/main.py
@@ -3,16 +3,12 @@ import time
 
 from functions import *
 
-A=np.random.randint(low=-2,high=5,size=(4,4))
-#A=np.array([[1,2,-3,4],[1,-4,2,-1]])
+A=np.array([[1,-8,-220],[81,2,380]])
 
 
 
-#X=triangularization(A,pivoting="integer",verbose=True,write_latex=True,index_start=1,\
-#    #latex_begin="\\begin{amatrix}{3}{1}",latex_end="\\end{amatrix}"
-#    )
-
-X=np.array([1,-4,3])
-Y=np.array([-2,-4,3])
-
-print(prod_vec(X,Y))
+X=triangularization(A,pivoting="integer",verbose=True,write_latex=True,index_start=1,\
+    latex_begin="\\begin{amatrix}{2}{1}",latex_end="\\end{amatrix}"
+    )
+print()
+print(X)
