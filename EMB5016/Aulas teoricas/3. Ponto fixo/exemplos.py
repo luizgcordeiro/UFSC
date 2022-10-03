f7a15e59522841e2ef0997d97c4f857d43992d38
Luiz Cordeiro <luizgc6@gmail.com>
2022-10-03 10:53:48 -0300

    commit

--- EMB5016/Aulas teoricas/3. Ponto fixo/exemplos.py
+++ EMB5016/Aulas teoricas/3. Ponto fixo/exemplos.py
@@ -4,7 +4,7 @@ def h(x): return 7-x**2+x
 def k(x): return (7-x**2)/(2*(x**2)+1) +x
 def n(x): return (x**2+7)/(2*x)
 
-# Teste para inverter numeros
+# Teste para inverter numeros 
 def g(x,c): return x*(2-c*x)
 
 # Teste criterios de parada e numero de iteracoes distintos
